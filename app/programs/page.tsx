b0caf6a025134e1f683052d894d93ba4d8691025
Alvin Fung <alvinfung0007@gmail.com>
2024-09-10 21:05:11 +1000

    Added programs page

---
+++ app/programs/page.tsx
@@ -0,0 +1,13 @@
+import Button from "../components/Buttons";
+import PageSection from "../components/PageSection";
+import Image from "next/image";
+import Page from "../page";
+
+export default function Programs() {
+  return (
+    <PageSection colourWay="dark">
+        Hello World
+    </PageSection>
+      
+  );
+}
