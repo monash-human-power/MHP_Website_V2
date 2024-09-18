252d0117dd30ddc8a3f491e52a8ae3d321536d6c
Alvin Fung <alvinfung0007@gmail.com>
2024-09-18 23:52:02 +1000

    Updated testimonials design

--- app/outreach/page.tsx
+++ app/outreach/page.tsx
@@ -4,6 +4,7 @@ import Button from "../components/Buttons";
 import PageSection from "../components/PageSection";
 import Image from "next/image";
 import programData from "../../public/JSONs/programs.json"; // Importing the JSON file
+import Page from "../page";
 
 export default function Outreach() {
   const [currentProgramIndex, setCurrentProgramIndex] = useState(0);
@@ -29,9 +30,8 @@ export default function Outreach() {
       setFadeClass("animate-fadeIn");
     }, 300); // Match the duration with the animation
   };
-  
-  const currentProgram = programData.programs[currentProgramIndex];
 
+  const currentProgram = programData.programs[currentProgramIndex];
 
   return (
     <>
@@ -141,7 +141,6 @@ export default function Outreach() {
         </section>
       </PageSection>
 
-      
       {/* Our Programs section */}
       <PageSection colourWay="dark">
         <div
@@ -235,26 +234,75 @@ export default function Outreach() {
             Testimonials
           </h2>
           <div
-            className="text-center text-xl grid grid-cols-2 gap-20"
+            className="text-center text-xl grid grid-cols-2 grid-rows-2 gap-x-20"
             style={{
               paddingTop: "40px",
               paddingLeft: "10%",
               paddingRight: "10%",
             }}
           >
-            <div>
-              <p>
-                "The kids loved it and the others that missed out were super
-                jealous!"
-                <br></br>- STEM Teacher at Westall Secondary College
-              </p>
+            <div className="max-w-screen-xl mx-auto text-center grid col-start-1 col-start-1 row-start-1 col-span-1">
+              <figure className="max-w-screen-md mx-auto">
+                <svg
+                  className="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600"
+                  viewBox="0 0 24 27"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
+                    fill="currentColor"
+                  />
+                </svg>
+                <blockquote>
+                  <p>
+                    "The kids loved it and the others that missed out were super
+                    jealous!"
+                  </p>
+                </blockquote>
+                <figcaption className="flex items-center justify-center mt-6 space-x-3">
+                  <div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+                    <div className="pr-3 font-medium text-gray-900 dark:text-white">
+                      STEM Teacher
+                    </div>
+                    <div className="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">
+                      Westall Secondary College
+                    </div>
+                  </div>
+                </figcaption>
+              </figure>
             </div>
-            <div>
-              <p>
-                "It was really fun! Had a blast being creative while teaching
-                important STEM skills."
-                <br></br>- Blake Haydon (MHP member)
-              </p>
+
+            <div className="max-w-screen-xl mx-auto text-center grid grid-cols-1 col-start-2 row-start-2 col-span-1">
+              <figure className="max-w-screen-md mx-auto">
+                <svg
+                  className="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600"
+                  viewBox="0 0 24 27"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
+                    fill="currentColor"
+                  />
+                </svg>
+                <blockquote>
+                  <p>
+                    "It was really fun! Had a blast being creative while
+                    teaching important STEM skills."
+                  </p>
+                </blockquote>
+                <figcaption className="flex items-center justify-center mt-6 space-x-3">
+                  <div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+                    <div className="pr-3 font-medium text-gray-900 dark:text-white">
+                      Blake Haydon
+                    </div>
+                    <div className="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">
+                      MHP member
+                    </div>
+                  </div>
+                </figcaption>
+              </figure>
             </div>
           </div>
         </div>
