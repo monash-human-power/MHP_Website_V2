634284674353a7b35dc8e248ddbefd6d58d814b1
Alvin Fung <alvinfung0007@gmail.com>
2024-09-19 23:38:18 +1000

    Updated outreach programs carousel

--- app/outreach/page.tsx
+++ app/outreach/page.tsx
@@ -77,9 +77,9 @@ export default function Outreach() {
           {/* Learn More button links to Our Programs section */}
           <Button hrefString="#ourPrograms" text="Learn More" theme="dark" />
         </div>
-
-        {/* Epsom Primary School */}
       </PageSection>
+
+      {/* Epsom Primary School */}
       <section className="relative text-center py-5 animate-fadeIn">
         <div className="items-center overflow-hidden mx-auto">
           <Image
@@ -94,52 +94,6 @@ export default function Outreach() {
         </div>
         <section id="ourPrograms"></section>
       </section>
-
-      {/* Sub-Programs navigation */}
-      <PageSection colourWay="dark">
-        <section className="flex justify-center items-center bg-gray-900">
-          <button
-            onClick={prevProgram}
-            className="z-10 p-4 bg-gray-300 rounded-full hover:bg-gray-400 text-4xl w-16 h-16 flex items-center justify-center"
-          >
-            &larr;
-          </button>
-          <h2 className="text-3xl font-bold mx-8 underline decoration-greenbutton">
-            {currentProgram.name}
-          </h2>
-          <button
-            onClick={nextProgram}
-            className="z-10 p-4 bg-gray-300 rounded-full hover:bg-gray-400 text-4xl w-16 h-16 flex items-center justify-center"
-          >
-            &rarr;
-          </button>
-        </section>
-
-        {/* Dynamic Program Section */}
-        <section className="">
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:space-x-8 px-8">
-            {/* Image */}
-            <div className="w-full lg:w-1/2">
-              <div className={`transition-opacity duration-300 ${fadeClass}`}>
-                <Image
-                  src={currentProgram.image} // Dynamically load the image from JSON
-                  alt={currentProgram.name}
-                  width={500}
-                  height={500}
-                  className="rounded-lg"
-                />
-              </div>
-            </div>
-
-            {/* Description */}
-            <div className="mt-2 lg:mt-0 lg:w-1/2">
-              <div className={`transition-opacity duration-300 ${fadeClass}`}>
-                <h2>{currentProgram.description}</h2>
-              </div>
-            </div>
-          </div>
-        </section>
-      </PageSection>
 
       {/* Our Programs section */}
       <PageSection colourWay="dark">
@@ -156,7 +110,7 @@ export default function Outreach() {
           </h2>
         </div>
         <div
-          className="text-xl"
+          className="flex justify-center items-center"
           style={{
             paddingTop: "20px",
             paddingBottom: "20px",
@@ -164,51 +118,52 @@ export default function Outreach() {
             margin: "0 auto",
           }}
         >
-          <p>
-            Our programs cover core concepts in areas like aerodynamics,
-            mechanical design, and rider development—each offering a unique
-            glimpse into the real-world applications of STEM.
-          </p>
-          <br></br>
-          <p>
-            For example, in the Aerodynamics Workshop, students learn how
-            airflow impacts speed and efficiency by designing and testing their
-            own aerodynamic models in a mini wind tunnel. They experiment with
-            different shapes, gaining insights into how engineers use
-            aerodynamics to create faster, more efficient vehicles.
-          </p>
-          <br></br>
-          <p>
-            The Rider Development Workshop introduces students to the science
-            behind human-powered racing, teaching them how to maximise energy
-            output and optimise race strategies through gear changes and racing
-            line techniques.
-          </p>
-          <br></br>
-          <p>
-            Through these workshops, MHP makes STEM approachable and engaging
-            for younger students, showing them how mathematics, physics, and
-            engineering can solve real-world challenges. By offering interactive
-            design sessions and testing phases, MHP gives students the chance to
-            apply their creativity, learn problem-solving skills, and experience
-            the thrill of innovation firsthand. These outreach efforts not only
-            spark curiosity but also help build a pipeline of future engineers,
-            scientists, and innovators.
-          </p>
-          <br></br>
-          <p>
-            You can enquire here to organise a workshop with your own school.
-          </p>
+          <button
+            onClick={prevProgram}
+            className="z-10 p-4 bg-gray-300 rounded-full hover:bg-gray-400 text-4xl w-16 h-16 flex items-center justify-center"
+          >
+            &larr;
+          </button>
+          <h2 className="text-3xl font-bold mx-8">{currentProgram.name}</h2>
+          <button
+            onClick={nextProgram}
+            className="z-10 p-4 bg-gray-300 rounded-full hover:bg-gray-400 text-4xl w-16 h-16 flex items-center justify-center"
+          >
+            &rarr;
+          </button>
         </div>
+
+        {/* Dynamic Program Section */}
         <div
-          className="flex flex-row justify-center items-center"
+          className="flex justify-center items-center"
           style={{
-            paddingTop: "20px",
+            paddingTop: "40px",
             paddingBottom: "20px",
+            width: "90%",
+            margin: "0 auto",
           }}
         >
-          {/* See Options button links to separate Programs page */}
-          <Button hrefString="/programs" text="See Options" theme="dark" />
+          <div className="flex flex-col lg:flex-row lg:space-x-8 px-8">
+            {/* Image */}
+            <div className="w-full lg:w-1/2">
+              <div className={`transition-opacity duration-300 ${fadeClass}`}>
+                <Image
+                  src={currentProgram.image} // Dynamically load the image from JSON
+                  alt={currentProgram.name}
+                  width={600}
+                  height={800}
+                  className="rounded-3xl"
+                />
+              </div>
+            </div>
+
+            {/* Description */}
+            <div className="mt-2 lg:mt-0 lg:w-1/2">
+              <div className={`transition-opacity duration-300 ${fadeClass}`}>
+                <p className="text-xl">{currentProgram.description}</p>
+              </div>
+            </div>
+          </div>
         </div>
       </PageSection>
 
@@ -308,11 +263,25 @@ export default function Outreach() {
         </div>
       </PageSection>
 
+      {/* Epsom Primary School */}
+      <section className="relative text-center py-5 animate-fadeIn">
+        <div className="items-center overflow-hidden mx-auto">
+          <Image
+            src="/outreach_3.jpg"
+            width="800"
+            height="300"
+            objectFit="cover"
+            alt="Epsom Primary School"
+            layout="responsive"
+          />
+        </div>
+      </section>
+
       {/* Find Out More Section */}
       <PageSection colourWay="dark">
         <div
           style={{
-            borderTop: "2px solid #B3B3B3", // Top border
+            // borderTop: "2px solid #B3B3B3", // Top border
             borderBottom: "2px solid #B3B3B3", // Bottom border
             width: "90%",
             margin: "0 auto",
@@ -321,7 +290,7 @@ export default function Outreach() {
         >
           <div
             style={{
-              padding: "40px",
+              // padding: "40px",
             }}
           >
             <h2 className="text-center text-3xl font-Sensation underline decoration-green decoration-4">
