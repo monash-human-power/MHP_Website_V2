15db1e953002f210bfd2cae13614abdb42747886
userr1010 <netharaathukorala10@gmail.com>
2024-09-20 23:58:14 +1000

    feat: all competitions added to page

--- app/competitions/page.tsx
+++ app/competitions/page.tsx
@@ -1,81 +1,148 @@
 import React from "react";
 import PageSection from "../components/PageSection";
-import Image from "next/image";
+import ImageCarousel from "../components/ImageCarousel";
+
+const hpvImages = ["/hpvImage1.jpg", "/hpvImage2.jpg", "/hpvImage3.jpg"];
+const bmImages = ["/bmImage1.jpg", "/bmImage2.jpg", "/bmImage3.jpg"];
+const ozhpvImages = ["/competitionImage2.png"];
 
 export default function Competitions() {
   return (
     <>
       <PageSection colourWay="dark">
         <div className="font-Aldrich bg-black text-white min-h-screen">
-          {/* Header */}
-          <div className="text-center mb-10">
-            <h1 className="text-4xl text-[#acf601]">Competitions</h1>
+          <div className="text-center">
+            <h1 className="text-4xl text-[#acf601] mb-5">Competitions</h1>
+            <p>
+              Monash Human Power participates in various competitions that
+              challenge the boundaries of human-powered vehicle innovation.
+              These events offer the team opportunities to showcase their
+              engineering prowess and push performance limits. With each
+              competition, Monash Human Power continues to refine its designs,
+              striving to break speed records and achieve new milestones in
+              human-powered transportation.
+            </p>
           </div>
 
           {/* World Human Powered Speed Challenge Section */}
-          <div className="flex flex-wrap justify-center items-center mb-10 px-5">
-            <div className="w-full lg:w-1/2 p-5">
-              <Image
+          <div className="mt-20">
+            <h1 className="text-center text-2xl text-[#acf601] mb-5">
+              World Human Powered Speed Challenge
+            </h1>
+            <div className="flex flex-col sm:flex-row flex-wrap justify-center items-center space-y-3 sm:space-y-0 sm:space-x-5 mb-5">
+              <p className="text-center border rounded-3xl border-[#acf601] p-3 sm:mb-0">
+                Max Speed: 144 kmph
+              </p>
+              <p className="text-center border rounded-3xl border-[#acf601] p-3 sm:mb-0">
+                More stats go here
+              </p>
+              <p className="text-center border rounded-3xl border-[#acf601] p-3 sm:mb-0">
+                More stats go here
+              </p>
+            </div>
+            <p className="mt-3">
+              The World Human Powered Speed Challenge, held annually on State
+              Route 305 in Battle Mountain, Nevada, attracts top teams worldwide
+              aiming to break the human-powered land speed record, which
+              currently stands at 144 km/h. This event presents a unique
+              combination of engineering and athleticism, where teams refine
+              their vehicle designs to reduce drag and maximize speed. In 2023,
+              Monash Human Power made history by surpassing the Australian men’s
+              single-track record, a significant milestone in their pursuit of
+              speed innovation.
+            </p>
+
+            <div className="flex flex-col sm:flex-row flex-wrap justify-center items-center mt-5">
+              {/* <Image
                 src="/competitionImage1.jpg"
                 layout="intrinsic"
                 height={400}
-                width={800}
+                width={400}
                 objectFit="cover"
                 alt="World Human Powered Speed Challenge"
               />
             </div>
             <div className="w-full lg:w-1/2 p-5">
-              <h2 className="text-2xl text-[#acf601]">
-                World Human Powered Speed Challenge
-              </h2>
-              <p className="mt-3">
-                The World Human Powered Speed Challenge takes place annually at
-                Battle Mountain, Nevada. Teams from around the world gather on
-                State Route 305 in a quest to break the human-powered land speed
-                record. Whilst the current world record is 144km/h, MHP’s
-                participation in 2023 led us to passing the Australian men’s
-                single-track record!
-              </p>
-              <a
-                href="https://www.ihpva.org/whpsc/index.htm"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <button
-                  type="submit"
-                  className="mt-5 px-5 py-2 rounded-md bg-greenbutton text-black border-black font-bold"
-                >
-                  Read More
-                </button>
-              </a>
+            <Image
+              src="/competitionImage1.jpg"
+              layout="intrinsic"
+              height={400}
+              width={400}
+              objectFit="cover"
+              alt="World Human Powered Speed Challenge"
+            /> */}
+              <div className="w-full lg:w-1/2 sm:h-1/2">
+                <ImageCarousel images={bmImages}></ImageCarousel>
+              </div>
             </div>
           </div>
 
           {/* OzHPV Speed Trials Section */}
-          <div className="flex flex-wrap justify-center items-center mb-10 px-5">
-            <div className="w-full lg:w-1/2 p-5">
-              <h2 className="text-2xl text-[#acf601]">OzHPV Speed Trials</h2>
-              <p className="mt-3">
-                The OzHPV Challenge is a series of races to determine the best
-                combination of human powered vehicle and rider. Its held at the
-                Ford Proving Grounds in You Yangs, Victoria. The current
-                Australian record for this competition is 96.68 km/h.
+          <div className="mt-20">
+            <h1 className="text-center text-2xl text-[#acf601] mb-5">
+              OzHPV Speed Trials
+            </h1>
+            <div className="flex flex-col sm:flex-row flex-wrap justify-center items-center space-y-3 sm:space-y-0 sm:space-x-5 mb-5">
+              <p className="text-center border rounded-3xl border-[#acf601] p-3 sm:mb-0">
+                More stats go here
               </p>
-              <a href="https://www.ozhpv.org.au/" target="_blank" rel="noopener noreferrer">
-              <button className="mt-5 px-5 py-2 rounded-md bg-greenbutton text-black hover:bg-green-700">
-                Read More
-              </button>
-              </a>
+              <p className="text-center border rounded-3xl border-[#acf601] p-3 sm:mb-0">
+                More stats go here
+              </p>
+              <p className="text-center border rounded-3xl border-[#acf601] p-3 sm:mb-0">
+                More stats go here
+              </p>
             </div>
-            <div className="w-full lg:w-1/2 p-5">
-              <Image
-                src="/competitionImage2.png"
-                layout="responsive"
-                height={50}
-                width={2000}
-                objectFit="cover"
-                alt="OzHPV Speed Trials"
-              />
+            <p className="mt-3">
+              The OzHPV Challenge is an exciting series of races designed to
+              test the best combinations of human-powered vehicles and rider
+              capabilities. Hosted at the Ford Proving Grounds in You Yangs,
+              Victoria, the trials push teams to fine-tune their designs for
+              peak performance under real-world conditions. The current
+              Australian record for this competition stands at 96.68 km/h, with
+              Monash Human Power competing to challenge this benchmark,
+              showcasing their commitment to developing world-class
+              human-powered vehicles.
+            </p>
+
+            <div className="flex flex-col sm:flex-row flex-wrap justify-center items-center mt-5">
+              <div className="w-full lg:w-1/2 sm:h-1/2">
+                <ImageCarousel images={ozhpvImages}></ImageCarousel>
+              </div>
+            </div>
+          </div>
+
+          {/* Vic HPV Grand Prix Series */}
+          <div className="mt-20 mb-5">
+            <h1 className="text-center text-2xl text-[#acf601] mb-5">
+              Vic HPV Grand Prix Series
+            </h1>
+            <div className="flex flex-col sm:flex-row flex-wrap justify-center items-center space-y-3 sm:space-y-0 sm:space-x-5 mb-5">
+              <p className="text-center border rounded-3xl border-[#acf601] p-3 sm:mb-0">
+                More stats go here
+              </p>
+              <p className="text-center border rounded-3xl border-[#acf601] p-3 sm:mb-0">
+                More stats go here
+              </p>
+              <p className="text-center border rounded-3xl border-[#acf601] p-3 sm:mb-0">
+                More stats go here
+              </p>
+            </div>
+            <p className="mt-3">
+              The VIC HPV Grand Prix Series is a 6-8 hour endurance race for
+              human-powered vehicles (HPVs) on a circuit, offering a unique
+              challenge compared to the short sprints of the World Human Powered
+              Speed Challenge (WHPSC). Unlike the speed-focused events, this
+              race tests vehicle durability and rider endurance over longer
+              distances. Monash Human Power has entered this competition with a
+              borrowed trike while developing its custom-designed trike, aiming
+              to push new boundaries in endurance HPV racing.
+            </p>
+
+            <div className="flex flex-col sm:flex-row flex-wrap justify-center items-center mt-5">
+              <div className="w-full lg:w-1/2 sm:h-1/2">
+                <ImageCarousel images={hpvImages}></ImageCarousel>
+              </div>
             </div>
           </div>
         </div>
