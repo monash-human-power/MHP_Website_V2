672353ec4404180c4fea3b63356410c81b0638da
Vicky Huang <vhua0005@student.monash.edu>
2024-09-23 18:56:01 +1000

    Added bike/trike images to the page - these will need to be resized accordingly

--- app/components/BikeSection.tsx
+++ app/components/BikeSection.tsx
@@ -1,17 +1,34 @@
 "use client"
 
 import React, {useState} from 'react'
+import Image from "next/image";
+
 import PageSection from './PageSection';
 import SubNavBar from "./SamePageNavigation/SubNavBar";
 import BikeContent from './BikeContent';
 
-import bikeData from "../../public/JSONs/bikes.json";  // Importing the JSON file
+import bikeDataJson from "../../public/JSONs/bikes.json";  // Importing the JSON file
 
 interface BikeSectionProps {
     bike: string
 }
 
+interface Bike {
+    image: string;
+    iteration: number;
+    max_speed: number;
+    development_start: number;
+    development_end: number | string; // to allow "current"
+    competitions: string[];
+    description: string;
+    gallery: string[];
+}
+
+type BikeData = Record<string, Bike>; // map of bike name to bike data
+
 const BikeSection = ({bike} : BikeSectionProps) => {
+    const bikeData: BikeData = bikeDataJson;
+    
     const sections = ["Overview", "Gallery"];
 
     // track the active section
@@ -25,6 +42,14 @@ const BikeSection = ({bike} : BikeSectionProps) => {
                         <h1 className="text-5xl font-bold">{bike}</h1>
                     </div>
                 </section>
+                <div className="relative w-full h-64">
+                <Image
+                    src={bikeData[bike].image}
+                    alt={`Image ${bike}`}
+                    layout="fill"
+                    objectFit="cover"
+                    />
+                </div>
                 {/* Insert bike image here */}
                 <SubNavBar 
                         sections={sections}
