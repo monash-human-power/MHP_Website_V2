4dcf2ec86b219b8f07497527f0d696ead3873687
mpha0039 <mpha0039@student.monash.edu>
2024-09-23 11:20:19 +1000

    fixed image not showing bug

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -110,7 +110,7 @@ export default function Outreach() {
               }}
             >
               <Image
-                src="/join_rider_dev.jpg"
+                src="/join/join_rider_dev.jpg"
                 width="200"
                 height="100"
                 objectFit="cover"
@@ -217,7 +217,7 @@ export default function Outreach() {
               }}
             >
               <Image
-                src="/join_aerodynamics.jpg"
+                src="/join/join_aerodynamics.jpg"
                 width="800"
                 height="300"
                 objectFit="cover"
@@ -340,7 +340,7 @@ export default function Outreach() {
               }}
             >
               <Image
-                src="/join_chassis_drivetrain.jpg"
+                src="/join/join_chassis_drivetrain.jpg"
                 width="800"
                 height="300"
                 objectFit="cover"
@@ -444,7 +444,7 @@ export default function Outreach() {
               }}
             >
               <Image
-                src="/join_operations.jpg"
+                src="/join/join_operations.jpg"
                 width="800"
                 height="300"
                 objectFit="cover"
@@ -572,7 +572,7 @@ export default function Outreach() {
               }}
             >
               <Image
-                src="/join_electrical.jpg"
+                src="/join/join_electrical.jpg"
                 width="800"
                 height="300"
                 objectFit="cover"
@@ -680,7 +680,7 @@ export default function Outreach() {
               }}
             >
               <Image
-                src="/join_materials.jpg"
+                src="/join/join_materials.jpg"
                 width="800"
                 height="300"
                 objectFit="cover"
