d93fa6f73733f0933a7fb53251e6f3ed10f1ded5
Amy T <amycaitjea@gmail.com>
2024-09-27 22:14:07 +1000

    Updating team text descriptions, images  and base p text size

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
       fontSize: {
         xs: ".75rem",
         sm: ".875rem",
-        base: "1rem",
+        base: "1.2rem",
         lg: "1.125rem",
         xl: "1.25rem",
         "2xl": "1.5rem",
