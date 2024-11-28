555d672c35da783207da1b9626c456fa44de0ab8
Amy T <amycaitjea@gmail.com>
2024-11-29 02:13:40 +1100

    Base Implementation of pdf viewing

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,24 +3,30 @@
 import PageSection from "../components/PageSection";
 import { useState } from "react";
 import BlogCard from "../components/BlogCard";
-import blogData from "../../public/JSONs/blogs.json";  // in json, have titles as keys
+import blogData from "../../public/JSONs/blogs.json"; // in json, have titles as keys
 
 type BlogData = {
   title: string;
-    image: string;
-    author: string;
-    date: string;
-    shortText: string;
-    fullText: string;
-    tags: { name: string; url: string }[];
+  image: string;
+  date: string;
+  description: string;
+  source: string;
+  tags: { name: string }[];
 };
 
 export default function Blog() {
+  // states for searching
   const [searchQuery, setSearchQuery] = useState("");
   const [searchTriggered, setSearchTriggered] = useState(false);
 
-  const articles:BlogData[] = Object.values(blogData);
+  //states for displaying newletters
+  // const [pdfDisplayed, setPdfDisplayed] = useState(false);
+  const [selectedBlog, setSelectedBlog] = useState<BlogData|null>(null);
 
+  const handleBlogClick = (article:BlogData) => {
+    setSelectedBlog(article); // Set the clicked blog as the selected blog
+  };
+  const articles: BlogData[] = Object.values(blogData);
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
@@ -28,13 +34,18 @@ export default function Blog() {
     }
   };
 
-  const filteredArticles = articles.filter((article) =>
-    article.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
-    article.tags.some((tag) => tag.name.toLowerCase().includes(searchQuery.toLowerCase()))
+  //todo - verify search on tags
+  const filteredArticles = articles.filter(
+    (article) =>
+      article.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      article.tags.some((tag) =>
+        tag.name.toLowerCase().includes(searchQuery.toLowerCase())
+      )
   );
 
   // Conditionally display filtered articles only after search is triggered, otherwise display all articles
-  const articlesToDisplay = searchTriggered && searchQuery ? filteredArticles : articles;
+  const articlesToDisplay =
+    searchTriggered && searchQuery ? filteredArticles : articles;
 
   return (
     <>
@@ -53,13 +64,50 @@ export default function Blog() {
             className="bg-black bg-opacity-70 border-2 border-[#ACF601] text-white placeholder-[#ACF601] rounded-lg w-80 py-3 px-4 text-base transition-shadow duration-300 focus:outline-none focus:ring-2 focus:ring-[#ACF601] focus:border-[#ACF601]"
           />
         </div>
-        <div className="relative">
-          <div className="flex overflow-x-scroll overflow-y-hidden  space-x-6" style={{ scrollbarWidth: 'thin', scrollbarColor: '#ACF601 black' }}>
+        <div className="relative bg-black">
+          <div
+            className="flex overflow-x-scroll overflow-y-hidden space-x-6"
+            style={{ scrollbarWidth: "thin", scrollbarColor: "#ACF601 black" }}
+          >
             {articlesToDisplay.map((article, index) => (
-              <BlogCard key={index} article={article} index={index} ></BlogCard>
-          ))}
+              <BlogCard
+                key={index}
+                article={article}
+                index={index}
+                onClick={() => handleBlogClick(article)}
+              />
+            ))}
           </div>
         </div>
+
+{/* PDF Embed */}
+{selectedBlog && (
+  <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-80 flex justify-center items-center z-50">
+    <div className="bg-black border border-green rounded-lg p-6 max-w-4xl w-full relative">
+      <button
+        className="absolute top-2 right-2 text-black"
+        onClick={() => setSelectedBlog(null)}
+      >
+        <p className="text-white">&#10006;</p>
+      </button>
+      <h2 className="text-2xl mb-4">{selectedBlog.title}</h2>
+      <div
+        className="relative w-full"
+        style={{
+          aspectRatio: '1 / 1.414', // Ensures proper A4 aspect ratio
+          maxWidth: '100%', // Prevents overflow on smaller screens
+          maxHeight: '90vh', // Restricts height to fit the viewport
+        }}
+      >
+        <iframe
+          src={selectedBlog.source}
+          className="absolute top-0 left-0 w-full h-full"
+          style={{ border: 'none' }} // Removes default iframe border
+        />
+      </div>
+    </div>
+  </div>
+)}
       </PageSection>
     </>
   );
--- app/components/BlogCard.tsx
+++ app/components/BlogCard.tsx
@@ -1,9 +1,10 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import Image from "next/image";
 
 interface BlogCardProps {
-    article: Article
-    index: number
+  article: Article;
+  index: number;
+  onClick: () => void;
 }
 
 // interface Article {
@@ -20,72 +21,73 @@ interface BlogCardProps {
 interface Article {
   title: string;
   image: string;
-  author: string;
   date: string;
-  shortText: string;
-  fullText: string;
-  tags: { name: string; url: string; }[];
+  description: string;
+  source: string;
+  tags: { name: string }[];
 }
 //todo
 
-const BlogCard = ({article,index} : BlogCardProps) => {
+const BlogCard = ({ article, index, onClick }: BlogCardProps) => {
+  const [showArticle, setShowFullText] = useState<{ [key: number]: boolean }>(
+    {}
+  );
 
-const [showFullText, setShowFullText] = useState<{ [key: number]: boolean }>({});
-  
-const toggleFullText = (index: number) => {
-  setShowFullText((prevState) => ({
-    ...prevState,
-    [index]: !prevState[index],
-  }));
-};
+  const toggleFullText = (index: number) => {
+    setShowFullText((prevState) => ({
+      ...prevState,
+      [index]: !prevState[index],
+    }));
+  };
 
-    return (
-        <>
-        <div
-          key={index}
-          className="flex-none w-full md:w-1/3 bg-black rounded-lg shadow-lg overflow-hidden transform transition-transform duration-300 hover:scale-105 hover:shadow-xl"
-          style={{ minHeight: showFullText[index] ? "auto" : "500px" }}  // Adjust the card's height dynamically
-        >
-                <Image
-                  src={article.image}
-                  width={1000}
-                  height={500}
-                  alt={article.title}
-                  className="w-full h-64 object-cover"
-                />
-                <div className="p-6">
-                  <h2 className="text-3xl font-semibold text-purple-700 decoration-purple-400 decoration-4 mb-4 drop-shadow-md">
-                    {article.title}
-                  </h2>
-                  <div className="mb-4">
-                    {article.tags.map((tag, idx) => (
-                      <a
-                        key={idx}
-                        href={tag.url}
-                        className="inline-block bg-[#ACF601] text-black text-xs font-medium rounded-full px-3 py-1 mr-2 mb-2"
-                      >
-                        {tag.name}
-                      </a>
-                    ))}
-                  </div>
-                  {/* Show full text or truncated version */}
-                  <p className={`text-gray-300 mb-4 ${showFullText[index] ? "block" : "line-clamp-3"} md:line-clamp-none`}>
-                    {showFullText[index] ? article.fullText : article.shortText}
-                  </p>
-                  {/* Toggle for read more / show less */}
+  return (
+    <>
+      <div
+        key={index}
+        className="flex-none w-full md:w-1/3 bg-black rounded-lg shadow-lg overflow-hidden transform transition-transform duration-300 hover:scale-105 hover:shadow-xl"
+        style={{ minHeight: "auto" }} // Adjust the card's height dynamically
+        onClick={onClick}
+      >
+        <Image
+          src={article.image}
+          width={1000}
+          height={500}
+          alt={article.title}
+          className="w-full h-64 object-cover"
+        />
+        <div className="p-6">
+          <h2 className="text-3xl font-semibold text-purple-700 decoration-purple-400 decoration-4 mb-4 drop-shadow-md">
+            {article.title}
+          </h2>
+          <div className="mb-4">
+            {article.tags.map((tag, idx) => (
+              <a
+                key={idx}
+                // todo: have when tag which clicked to show only articles with that tag
+                className="inline-block bg-[#ACF601] text-black text-xs font-medium rounded-full px-3 py-1 mr-2 mb-2"
+              >
+                {tag.name}
+              </a>
+            ))}
+          </div>
+          {/* Show full text or truncated version */}
+          <p className={`text-gray-300 mb-4 "line-clamp-3" md:line-clamp-none`}>
+            {article.description}
+          </p>
+          {/* Toggle for read more / show less
                   <h4
                     className="text-lg text-green-600 decoration-green-400 decoration-4 mb-2 cursor-pointer hover:text-green-800 transition-colors duration-200"
                     onClick={() => toggleFullText(index)}
                   >
                     <i className="fas fa-arrow-right"></i> {showFullText[index] ? "Show less" : "Read more"}
-                  </h4>
-                  <p className="text-gray-600">{article.author} • {article.date}</p>
-                </div>
-              </div>
-        </>
-    );
-}
+                  </h4> */}
+          <p className="text-gray-600">
+             {article.date}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default BlogCard;
-
-
