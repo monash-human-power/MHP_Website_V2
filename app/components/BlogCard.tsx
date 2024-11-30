5a8b634a1df43ba99887346ac28e054cd74065a5
Amy T <amycaitjea@gmail.com>
2024-11-30 23:22:37 +1100

    Cleaning up css

--- app/components/BlogCard.tsx
+++ app/components/BlogCard.tsx
@@ -7,17 +7,6 @@ interface BlogCardProps {
   onClick: () => void;
 }
 
-// interface Article {
-//     image: string;
-//     iteration: number;
-//     max_speed: number;
-//     development_start: number;
-//     development_end: number | string; // to allow "current"
-//     competitions: string[];
-//     description: string;
-//     gallery: string[];
-// }
-
 interface Article {
   title: string;
   image: string;
@@ -44,7 +33,7 @@ const BlogCard = ({ article, index, onClick }: BlogCardProps) => {
     <>
       <div
         key={index}
-        className="flex-none w-full md:w-1/3 bg-black rounded-lg shadow-lg overflow-hidden transform transition-transform duration-300 hover:scale-105 hover:shadow-xl"
+        className="flex-none w-full  md:w-1/3 bg-black rounded-lg shadow-lg overflow-hidden transform transition-transform duration-300 hover:scale-105 hover:shadow-xl"
         style={{ minHeight: "auto" }} // Adjust the card's height dynamically
         onClick={onClick}
       >
@@ -70,17 +59,9 @@ const BlogCard = ({ article, index, onClick }: BlogCardProps) => {
               </a>
             ))}
           </div>
-          {/* Show full text or truncated version */}
           <p className={`text-gray-300 mb-4 "line-clamp-3" md:line-clamp-none`}>
             {article.description}
           </p>
-          {/* Toggle for read more / show less
-                  <h4
-                    className="text-lg text-green-600 decoration-green-400 decoration-4 mb-2 cursor-pointer hover:text-green-800 transition-colors duration-200"
-                    onClick={() => toggleFullText(index)}
-                  >
-                    <i className="fas fa-arrow-right"></i> {showFullText[index] ? "Show less" : "Read more"}
-                  </h4> */}
           <p className="text-gray-600">
              {article.date}
           </p>
--- app/news/page.tsx
+++ app/news/page.tsx
@@ -21,9 +21,9 @@ export default function News() {
 
   //states for displaying newletters
   // const [pdfDisplayed, setPdfDisplayed] = useState(false);
-  const [selectedBlog, setSelectedBlog] = useState<Article|null>(null);
+  const [selectedBlog, setSelectedBlog] = useState<Article | null>(null);
 
-  const handleBlogClick = (article:Article) => {
+  const handleBlogClick = (article: Article) => {
     setSelectedBlog(article); // Set the clicked blog as the selected blog
   };
   const articles: Article[] = Object.values(blogData);
@@ -54,60 +54,65 @@ export default function News() {
           News
         </h1>
         {/* Search bar */}
-        <div className="mb-8">
+        <div className="mb-8 flex justify-start">
           <input
             type="text"
             placeholder="Search by headline or tag..."
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)} // Capture search query
             onKeyDown={handleKeyDown} // Handle "Enter" key press
-            className="bg-black bg-opacity-70 border-2 border-[#ACF601] text-white placeholder-[#ACF601] rounded-lg w-80 py-3 px-4 text-base transition-shadow duration-300 focus:outline-none focus:ring-2 focus:ring-[#ACF601] focus:border-[#ACF601]"
+            className="font-Sansation bg-black bg-opacity-70 border-2 border-[#ACF601] text-white placeholder-[#ACF601] rounded-lg w-80 py-3 px-4 text-base transition-shadow duration-300 focus:outline-none focus:ring-2 focus:ring-[#ACF601] focus:border-[#ACF601]"
           />
         </div>
         <div className="relative bg-black">
           <div
-            className="flex overflow-x-scroll overflow-y-hidden space-x-6"
+            className="flex justify-evenly overflow-x-auto overflow-y-hidden space-x-6"
             style={{ scrollbarWidth: "thin", scrollbarColor: "#ACF601 black" }}
           >
-            {articlesToDisplay.map((article, index) => (
-              <BlogCard
-                key={index}
-                article={article}
-                index={index}
-                onClick={() => handleBlogClick(article)}
-              />
-            ))}
+            {//if no articles match search display nothing
+              articlesToDisplay.length === 0 ? (
+              <p>No articles match your query</p>
+            ) : (
+              articlesToDisplay.map((article, index) => (
+                <BlogCard
+                  key={index}
+                  article={article}
+                  index={index}
+                  onClick={() => handleBlogClick(article)}
+                />
+              ))
+            )}
           </div>
         </div>
 
-{/* PDF Embed */}
-{selectedBlog && (
-  <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-80 flex justify-center items-center z-50">
-    <div className="bg-black border border-green rounded-lg p-6 max-w-4xl w-full relative">
-      <button
-        className="absolute top-2 right-2 text-black"
-        onClick={() => setSelectedBlog(null)}
-      >
-        <p className="text-white">&#10006;</p>
-      </button>
-      <h2 className="text-2xl mb-4">{selectedBlog.title}</h2>
-      <div
-        className="relative w-full"
-        style={{
-          aspectRatio: '1 / 1.414', // Ensures proper A4 aspect ratio
-          maxWidth: '100%', // Prevents overflow on smaller screens
-          maxHeight: '90vh', // Restricts height to fit the viewport
-        }}
-      >
-        <iframe
-          src={selectedBlog.source}
-          className="absolute top-0 left-0 w-full h-full"
-          style={{ border: 'none' }} // Removes default iframe border
-        />
-      </div>
-    </div>
-  </div>
-)}
+        {/* PDF Embed */}
+        {selectedBlog && (
+          <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-80 flex justify-center items-center z-50">
+            <div className="bg-black border border-green rounded-lg p-6 max-w-4xl w-full relative">
+              <button
+                className="absolute top-2 right-2 text-black"
+                onClick={() => setSelectedBlog(null)}
+              >
+                <p className="text-white">&#10006;</p>
+              </button>
+              <h2 className="text-2xl mb-4">{selectedBlog.title}</h2>
+              <div
+                className="relative w-full"
+                style={{
+                  aspectRatio: "1 / 1.414", // Ensures proper A4 aspect ratio
+                  maxWidth: "100%",
+                  maxHeight: "80vh",
+                }}
+              >
+                <iframe
+                  src={selectedBlog.source}
+                  className="absolute top-0 left-0 w-full h-full"
+                  style={{ border: "none" }} // Removes default iframe border
+                />
+              </div>
+            </div>
+          </div>
+        )}
       </PageSection>
     </>
   );
