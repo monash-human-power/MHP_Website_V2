30028c10ce3fb3afa51b64851a02326b91fd75e6
jeongwoo <jeongwoo.2xadvisory@gmail.com>
2025-09-29 11:05:03 +1000

    Feature: Integrated ScrollProgressBar

--- app/components/SamePageNavigation/SamePageNavBar.tsx
+++ app/components/SamePageNavigation/SamePageNavBar.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Link from "next/link";
+import ScrollProgressBar from "../ScrollProgressBar";
 
 interface SamePageNavBarProps {
     sections: string[]; // list of the sections to scroll to
@@ -11,6 +12,9 @@ interface SamePageNavBarProps {
 const SamePageNavBar = ({ sections , activeSection, setActiveSection} : SamePageNavBarProps) => {
     return (
         <>
+        <div className="fixed top-0 left-0 w-full h-2 bg-red-500 z-[9999] opacity-100">
+            <ScrollProgressBar />
+        </div>
         <nav className="sticky top-28 w-full bg-black text-white font-Aldrich xl:text-xl lg:text-l z-50">
             <div className="hidden lg:block font-Aldrich">
             <div className="flex justify-center items-center h-full w-full space-x-10 pl-4 pt-2 pb-2">
